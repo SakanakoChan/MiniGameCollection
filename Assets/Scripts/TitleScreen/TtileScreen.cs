3d670bda7ede52b49896aab2fa5d8df3809f7a78
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-09-17 17:08:59 +0800

    Added Title Screen

---
+++ Assets/Scripts/TitleScreen/TtileScreen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TtileScreen : MonoBehaviour
+{
+    public void LoadLevel(string _levelName)
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+}
