fb80e8f5c1c7280646e74d9e6da787d9c4137165
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-09-17 22:26:53 +0800

    Added scroll page effect for title screen

---
+++ Assets/Scripts/TitleScreen/PageEffect.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PageEffect : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private GameObject content;
+
+    private ScrollRect scroll;
+    private bool isDraging;
+
+    private float pageWidthPercentage;
+    private int pageAmount;
+
+    private int targetPageIndex = 0;
+
+
+    private void Awake()
+    {
+        scroll = GetComponent<ScrollRect>();
+    }
+
+    private void Start()
+    {
+        pageAmount = content.transform.childCount;
+        //minus 1 here because there will be gaps between pages, these gaps should also get taken into account of the page size
+        //or in other words, page 1 to page 5, user should swipe 4 times
+        //and if don't minues 1 here, user should swipe 5 times, which is wrong
+        pageWidthPercentage = 1f / (pageAmount - 1); 
+
+        for (int i = 0; i < pageAmount; i++)
+        {
+            Debug.Log(i * pageWidthPercentage);
+        }
+    }
+
+    private void Update()
+    {
+        if (!isDraging && scroll.horizontalNormalizedPosition != targetPageIndex * pageWidthPercentage)
+        {
+            scroll.horizontalNormalizedPosition = Mathf.Lerp(scroll.horizontalNormalizedPosition, targetPageIndex * pageWidthPercentage, 10 * Time.deltaTime);
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDraging = true;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDraging = false;
+
+        float currentHorizontalPostionPercentage = scroll.horizontalNormalizedPosition;
+        float closetPageDistancePercentage = Mathf.Abs(currentHorizontalPostionPercentage - 0);
+
+        for (int i = 0; i < pageAmount; i++)
+        {
+            float distancePercentageToCurrentPage = Mathf.Abs(currentHorizontalPostionPercentage - i * pageWidthPercentage);
+
+            if (distancePercentageToCurrentPage <= closetPageDistancePercentage)
+            {
+                closetPageDistancePercentage = distancePercentageToCurrentPage;
+                targetPageIndex = i;
+            }
+        }
+    }
+
+}
