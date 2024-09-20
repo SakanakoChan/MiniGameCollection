da7d304088a0d50361acf63d67a2a9e31831c2fb
SakanakoChan <148366895+SakanakoChan@users.noreply.github.com>
2024-09-20 18:28:20 +0800

    building android program...

--- Assets/Scripts/CallJava.cs
+++ Assets/Scripts/CallJava.cs
@@ -1,24 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngineInternal;
 
 public class CallJava : MonoBehaviour
 {
     AndroidJavaClass javaClass = null;
     AndroidJavaObject javaObject = null;
 
+    AndroidJavaClass unityPlayer = null;
+    AndroidJavaObject currentActivity = null;
+
     [SerializeField] private TMP_InputField logInputField;
     [SerializeField] private TMP_InputField logPrint;
     [SerializeField] private TMP_InputField nameInputField;
     [SerializeField] private TMP_InputField namePrint;
+
+    [SerializeField] private TMP_InputField messageInputField;
 
 
     void Start()
     {
         javaClass = new AndroidJavaClass("com.example.unitytest.Test");
         javaObject = new AndroidJavaObject("com.example.unitytest.Test");
+
+        unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
     }
 
     public void SetLOG()
@@ -63,5 +68,10 @@ public class CallJava : MonoBehaviour
     {
         string name = javaObject.Get<string>("name");
         namePrint.text = name;
+    }
+
+    public void ShowMessage()
+    {
+        javaObject.Call("ShowMessage", messageInputField.text);
     }
 }
